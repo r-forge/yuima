1546686ac0d7730ac50a981851bbd0c902a7092f
phoenix844 (Emanuele Guidotti) <emanuele.guidotti@hotmail.it>
2016-11-15 22:29:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yuima@522 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yuimaGUI/inst/yuimaGUI/server.R
+++ pkg/yuimaGUI/inst/yuimaGUI/server.R
@@ -35,7 +35,7 @@ server <- function(input, output, session) {
   
   ### Home
   output$video_intro <- renderUI({
-    HTML('<iframe width="90%" height="250px" src="//www.youtube.com/embed/XX_bmCrI_gc" frameborder="0" allowfullscreen></iframe>')
+    HTML('<iframe width="90%" height="250px" src="https://www.youtube.com/embed/XX_bmCrI_gc?rel=0" frameborder="0" allowfullscreen></iframe>')
   })
   
   output$certificates <- renderUI({
