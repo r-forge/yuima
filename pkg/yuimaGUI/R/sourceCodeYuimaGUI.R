c73f215e3c12f2bef3201f105e05465f5e577819
phoenix844 (Emanuele Guidotti) <emanuele.guidotti@hotmail.it>
2017-01-10 06:10:05 +0000

    white theme now available
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yuima@555 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yuimaGUI/R/sourceCodeYuimaGUI.R
+++ pkg/yuimaGUI/R/sourceCodeYuimaGUI.R
@@ -1,15 +1,16 @@
-yuimaGUI <- function(theme = "last") {
+yuimaGUI <- function(theme = NULL) {
 
   print("Please wait while loading...")
   addr <- system.file("yuimaGUI", package = "yuimaGUI")
   
-  if(theme!="last"){
+  if(!is.null(theme)){
     addr_from <- paste(addr, "/www/", theme, ".css", sep = "")
     addr_to <- paste(addr, "/www/custom.css", sep = "")
     if(file.exists(addr_from))
       file.copy(from = addr_from, to = addr_to, overwrite = TRUE)
-    else print("Theme not supported.")
+    else print("Theme not supported. Using default.")
   }
+  
   
   utils::capture.output(
     suppressWarnings(
