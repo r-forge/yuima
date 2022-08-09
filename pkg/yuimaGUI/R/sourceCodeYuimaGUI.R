5e5ede6424815f40e2260b0c7cd8e147acce6589
phoenix844 (Emanuele Guidotti) <emanuele.guidotti@hotmail.it>
2022-08-09 20:08:28 +0000

    Fix CRAN version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yuima@808 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yuimaGUI/R/sourceCodeYuimaGUI.R
+++ pkg/yuimaGUI/R/sourceCodeYuimaGUI.R
@@ -1,3 +1,20 @@
+#' User-friendly Interface for the yuima package
+#' 
+#' Runs yuima Graphical User Interface
+#' 
+#' @param theme GUI theme: "black" or "white".
+#' 
+#' @return Starts yuima GUI
+#' 
+#' @author The YUIMA Project Team
+#' 
+#' @examples 
+#' \dontrun{
+#' yuimaGUI()
+#' }
+#' 
+#' @export
+#' 
 yuimaGUI <- function(theme = "black") {
 
   if(!(theme %in% c("black", "white"))) stop ("Theme not supported. Only 'black' or 'white' themes are available.")
